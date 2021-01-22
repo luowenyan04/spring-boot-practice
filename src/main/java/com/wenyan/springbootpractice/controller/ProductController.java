c2a04ecdefeeda621509ff1feba8412f16e99ebc
luowenyan04 <luowenyan04@gmail.com>
2021-01-22 14:29:40 +0800

    https://medium.com/chikuwa-tech-study/spring-boot-%E7%AC%AC1%E8%AA%B2-%E5%BB%BA%E7%AB%8B%E5%88%9D%E5%A7%8B%E5%B0%88%E6%A1%88-214ff084f787
    第1課-建立初始專案

---
+++ src/main/java/com/wenyan/springbootpractice/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.wenyan.springbootpractice.controller;
+
+import com.wenyan.springbootpractice.entity.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class ProductController {
+
+    private final List<Product> productDB = new ArrayList<>();
+
+    @PostConstruct
+    private void initDB() {
+        productDB.add(new Product("B0001", "Android Development (Java)", 380));
+        productDB.add(new Product("B0002", "Android Development (Kotlin)", 420));
+        productDB.add(new Product("B0003", "Data Structure (Java)", 250));
+        productDB.add(new Product("B0004", "Finance Management", 450));
+        productDB.add(new Product("B0005", "Human Resource Management", 330));
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getProduct(@PathVariable("id") String id) {
+        Optional<Product> productOp = productDB.stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst();
+
+        if (!productOp.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Product product = productOp.get();
+
+        return ResponseEntity.ok().body(product);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product request) {
+        boolean isIdDuplicated = productDB.stream().anyMatch(product -> product.getId().equals(request.getId()));
+        if (isIdDuplicated) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        Product product = new Product();
+        product.setId(request.getId());
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        productDB.add(product);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id")
+                .buildAndExpand(product.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(product);
+    }
+}
